74efde015ebaf2200102e8e00d61884856d33a86
Todd Trimble <todd.trimble@gmail.com>
2014-12-03 22:20:41 -0500

    MiniTest? (Seeting up TravisCI)

--- test/test_ingress_info.rb
+++ test/test_ingress_info.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'ingress_info'
 
-class TestIngressInfo < Minitest::Unit::TestCase
+class TestIngressInfo < MiniTest::Unit::TestCase
   def test_that_checkpoints_can_include_zero
     set = IngressCycles.checkpointsForDate(Time.new(2014, 12, 02, 00, 00, 00), Date.new(2014, 12, 02))
     assert_equal "00:00", set[0]
